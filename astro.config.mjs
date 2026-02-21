c4cbc920e985648bb0b13e4c58119624c37858b7
Zivalez <adit050604@gmail.com>
2026-02-22 03:32:45 +0700

    feat: switch to SSR for instant markdown updates from R2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
+    output: 'server',
     adapter: cloudflare(),
     vite: {
         define: {
